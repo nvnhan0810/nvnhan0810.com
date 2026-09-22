66058c70d7ddca1c52acbbbd5194f58ac9bf86d7
nvnhan0810 <nguyenvannhan0810@gmail.com>
2026-09-22 14:14:08 +0000

    Fix job promodoro error job

--- index/app/Jobs/Todo/SendPomodoroPhasePushJob.php
+++ index/app/Jobs/Todo/SendPomodoroPhasePushJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs\Todo;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Modules\Todo\Application\DeliverPomodoroPhasePush;
+use Modules\Todo\Domain\PomodoroDefaults;
 
 final class SendPomodoroPhasePushJob implements ShouldQueue
 {
@@ -12,11 +13,26 @@ final class SendPomodoroPhasePushJob implements ShouldQueue
 
     public int $tries = 2;
 
+    public int $userId;
+
+    public int $expectedEndsAtMs;
+
+    /**
+     * Default keeps delayed jobs serialized before fromPhase was added runnable.
+     */
+    public string $fromPhase = PomodoroDefaults::PHASE_FOCUS;
+
     public function __construct(
-        public readonly int $userId,
-        public readonly int $expectedEndsAtMs,
-        public readonly string $fromPhase,
-    ) {}
+        int $userId,
+        int $expectedEndsAtMs,
+        string $fromPhase = PomodoroDefaults::PHASE_FOCUS,
+    ) {
+        $this->userId = $userId;
+        $this->expectedEndsAtMs = $expectedEndsAtMs;
+        $this->fromPhase = in_array($fromPhase, PomodoroDefaults::PHASES, true)
+            ? $fromPhase
+            : PomodoroDefaults::PHASE_FOCUS;
+    }
 
     public function handle(DeliverPomodoroPhasePush $deliver): void
     {
