75cd766835495e66a2de7eb37c446fdbfbae02ff
nvnhan0810 <nguyenvannhan0810@gmail.com>
2026-09-22 02:47:48 +0000

    Implement Backlog To Matrix Feature

--- index/routes/todo.php
+++ index/routes/todo.php
@@ -9,6 +9,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/matrix', [MatrixController::class, 'index'])->name('matrix.index');
     Route::get('/matrix/stream', [MatrixController::class, 'stream'])->name('matrix.stream');
     Route::patch('/matrix/todos/{id}', [MatrixController::class, 'update'])->name('matrix.update');
+    Route::post('/matrix/backlog/promote', [MatrixController::class, 'promoteBacklog'])->name('matrix.backlog.promote');
 });
 
 Route::middleware('auth')->prefix('todos')->name('todos.')->group(function () {
